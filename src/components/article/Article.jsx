f32217def6f8985dc8f1516cf505eeb94e6d8e84
Francisco Neto <f.neto@outlook.com>
2024-07-03 22:08:11 -0300

    Alterando alguns componentes para componentes funcionais

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-import articleImg1 from "../../assets/images/article1.png";
+// import articleImg1 from "../../assets/images/article1.png";
 import "./styles.css";
 
-export class Article extends React.Component {
-  render() {
-    return (
-      <article id="article">
-        <img src={this.props.thumbnail} alt={this.props.title} />
-        <div className="article-infos">
-          <h2>{this.props.title}</h2>
-          <h3>{this.props.provider}</h3>
-          <p>{this.props.description}</p>
-        </div>
-      </article>
-    );
-  }
+/**
+ * * As props na função a seguir estão sendo recebidas desestruturadas, e não um objeto props, que também pode ser passado por parâmetro.
+ * * porém, ao passar o parametro props por parametro, ele virá como objeto e será necessário acessar as propriedades com o props. antes.
+ * * ex: props.thumbnail, props.title...
+ * @param {*} param0
+ * @returns
+ */
+export function Article({ thumbnail, title, provider, description }) {
+  return (
+    <article id="article">
+      <img src={thumbnail} alt={title} />
+      <div className="article-infos">
+        <h2>{title}</h2>
+        <h3>{provider}</h3>
+        <p>{description}</p>
+      </div>
+    </article>
+  );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,33 +2,32 @@ import React from "react";
 import logoImg from "../../assets/images/logo.png";
 import "./styles.css";
 
-class Navbar extends React.Component {
-  render() {
-    return (
-      <header>
-        <nav id="navbar">
-          <div className="nav-brand">
-            <img src={logoImg} alt="" />
-            <h1>Space Flight News</h1>
-          </div>
-          <ul className="nav-list">
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">Tranding</a>
-            </li>
-            <li>
-              <a href="#">Catgories</a>
-            </li>
-            <li>
-              <a href="#">About us</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
+// class Navbar extends React.Component {
+export function Navbar() {
+  return (
+    <header>
+      <nav id="navbar">
+        <div className="nav-brand">
+          <img src={logoImg} alt="" />
+          <h1>Space Flight News</h1>
+        </div>
+        <ul className="nav-list">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Tranding</a>
+          </li>
+          <li>
+            <a href="#">Catgories</a>
+          </li>
+          <li>
+            <a href="#">About us</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-export default Navbar;
+// export default Navbar;
